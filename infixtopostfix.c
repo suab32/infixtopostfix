701b6bcbdfd949d2509f3c48faabb32ea3c00b56
suab32 <workfor9051571833@gmail.com>
2017-07-22 10:22:25 +0000

    Update infixtopostfix.c

--- infixtopostfix.c
+++ infixtopostfix.c
@@ -150,8 +150,8 @@ int main()
     char p[50],*q;
     int choice,item;
     struct stack *STACK;
-    int s=1;
-    while(s)
+    int t=1;
+    while(t)
     {
         printf("\n1.to convert infix to postfix\n");
         printf("2.to evaluate postfix\n");
@@ -179,6 +179,6 @@ int main()
                 break;
         }
         printf("1.continue 2.terminate\n");
-       scanf("%d",&s);
+       scanf("%d",&t);
     }
 }
